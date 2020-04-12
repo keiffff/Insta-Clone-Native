e997410f21d5b6ca5b89fe81f50316bd2f8bcdc1
kei <tenguuenglish@gmail.com>
2020-04-12 10:08:01 +0900

    :tada: Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,75 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    'prettier/react',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'react', 'react-hooks'],
+  rules: {
+    // eslint
+    'global-require': 'off',
+    'consistent-return': 'off',
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'import/prefer-default-export': 'off',
+    'no-restricted-syntax': 'off',
+    'no-unused-expressions': ['error', {allowShortCircuit: true}],
+    'no-underscore-dangle': 'off',
+    'no-nested-ternary': 'off',
+    // @typescript-eslint
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    //prettier
+    'prettier/prettier': [
+      'warn',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    // import
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    // react
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/jsx-one-expression-per-line': 'warn',
+    'react/no-array-index-key': 'off',
+    'react/jsx-wrap-multilines': 'off',
+    // react-hooks
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
