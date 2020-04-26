e22b27b6329315f16804cad27db96b6d2fc0ee0c
kei <tenguuenglish@gmail.com>
2020-04-26 12:42:36 +0900

    :heavy_plus_sign: Add react-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Auth0Provider } from './providers/Auth0';
 import { Home } from './screens/Home';
 
+const Stack = createStackNavigator();
+
 export const App = () => (
   <Auth0Provider>
-    <Home />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   </Auth0Provider>
 );
