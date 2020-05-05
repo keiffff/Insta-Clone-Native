9cc173074490af825eba46cb5c1569ccbee42c13
kei <tenguuenglish@gmail.com>
2020-05-05 23:47:19 +0900

    :lipstick: Add loading view to home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo, useCallback, ComponentProps } from 'react';
 import { useAuth0 } from 'providers/Auth0';
 import { PostsList } from 'components/PostsList';
+import { LoadingView } from 'components/LoadingView';
 import { useDeleteLikeMutation, useNotifyNewPostsSubscription, useInsertLikeMutation } from 'types/hasura';
 import { StackHeaderOptions } from '@react-navigation/stack/lib/typescript/src/types';
 import { Logo } from './Logo';
@@ -9,7 +10,9 @@ import { CameraButton } from './CameraButton';
 
 export const Home = () => {
   const currentUser = useAuth0();
-  const { data: notifyNewPostsData } = useNotifyNewPostsSubscription({ variables: { userId: currentUser.sub } });
+  const { loading: notifyNewPostsDataLoading, data: notifyNewPostsData } = useNotifyNewPostsSubscription({
+    variables: { userId: currentUser.sub },
+  });
   const [insertLike] = useInsertLikeMutation();
   const [deleteLike] = useDeleteLikeMutation();
   const posts = useMemo(
@@ -43,7 +46,7 @@ export const Home = () => {
     [currentUser.sub, insertLike, deleteLike],
   );
 
-  return <PostsList posts={posts} onPress={handlePressPostItem} />;
+  return notifyNewPostsDataLoading ? <LoadingView /> : <PostsList posts={posts} onPress={handlePressPostItem} />;
 };
 
 export const HomeOptions: StackHeaderOptions = {
