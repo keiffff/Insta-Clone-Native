2ea89c606693c19c83b22fabb7fbd6f87a90d139
kei <tenguuenglish@gmail.com>
2020-05-05 15:16:14 +0900

    :bug: Fix to display image from google cloud

--- src/components/PostsList/index.stories.tsx
+++ src/components/PostsList/index.stories.tsx
@@ -15,20 +15,29 @@ storiesOf('PostsList', module)
 const dummyPosts = [
   {
     id: 1,
-    image: 'https://picsum.photos/id/237/200/300',
+    image: 'https://storage.googleapis.com/image-processor-app/iOS の画像.jpg',
     user: {
-      id: 'google-oauth2|116031656602944320296',
-      name: 'Kei',
-      avatar: 'https://picsum.photos/seed/picsum/200/300',
+      id: 'google-oauth2|104723435908719379671',
+      name: 'Kei sub',
+      avatar: 'https://storage.googleapis.com/image-processor-app/51147FD4-5199-4073-BF14-88C39812106A.jpeg',
     },
   },
   {
     id: 2,
-    image: 'https://picsum.photos/id/237/200/300',
+    image: 'https://storage.googleapis.com/image-processor-app/46C493F4-4703-4A4A-8D7F-60F2B8D37604.jpeg',
     user: {
       id: 'google-oauth2|116031656602944320296',
       name: 'Kei',
-      avatar: 'https://picsum.photos/seed/picsum/200/300',
+      avatar: 'https://storage.googleapis.com/image-processor-app/IMG_1087 (1).JPG',
+    },
+  },
+  {
+    id: 3,
+    image: 'https://storage.googleapis.com/image-processor-app/55C7EEE5-764A-4911-8435-7CB09EA384D7.jpeg',
+    user: {
+      id: 'google-oauth2|116031656602944320296',
+      name: 'Kei',
+      avatar: 'https://storage.googleapis.com/image-processor-app/IMG_1087 (1).JPG',
     },
   },
 ];
